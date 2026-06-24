c9bf01dae010291869aed7365a4ec4d79670dea7
Faizan <faizan.akhtar1221@gmail.com>
2026-06-24 12:07:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Card from "./components/card";
+
+function Button(){
+  return <button className="click">click it!</button>
+}
+
+function App(){
+  return <>
+  <div className="parents">
+    
+   </div>
+     <Card />
+     <Card />
+    <div>
+  </div>
+  </>
+}
+export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='Card'>
+        <img src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="person Photo" />
+        <h1>Adreson</h1>
+        <h3>Senior UI/UX Designer</h3>
+        <button>View Profile</button>
+    </div>
+  )
+}
+
+export default Card
