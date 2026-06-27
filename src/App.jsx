37c8fc5a8a0b806640708f0492c34550bbbb04b5
Faizan <faizan.akhtar1221@gmail.com>
2026-06-27 22:50:38 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import Card from "./components/card";
 
-function Button(){
-  return <button className="click">click it!</button>
+function Button() {
+  return <button className="click">click it!</button>;
 }
 
-function App(){
-  return <>
-  <div className="parents">
-    
-   </div>
-     <Card />
-     <Card />
-    <div>
-  </div>
-  </>
+function App() {
+  return (
+    <>
+      <div className="parents">
+      
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        
+      </div>
+      <div>
+        <p className="p">dkfkdfjkdjfkdk</p>
+        <Button />
+      </div>
+    </>
+  );
 }
 export default App;
